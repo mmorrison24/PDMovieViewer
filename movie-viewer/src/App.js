cc42886f4d3d63f929faa96aa77f96933889759e
Mathew Morrison <m@MacBook-Pro.local>
2019-12-10 07:48:47 -0500

    TMDB setup + Poser layout

--- movie-viewer/src/App.js
+++ movie-viewer/src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const tmdb_api_key = "cd140517203d1d5fbed8108980313abc";
 
 const Poster = ({ data }) => (
     <div
-        class="bg-gray-400 border m-2 rounded-lg shadow-md p-6"
+        class=" border m-2 rounded-lg shadow-md p-3"
         style={{
-            width: "185px"
+            width: "200px"
         }}
     >
         <img src={data} alt="poster"></img>
     </div>
 );
 
-function App() {
-  return (
-    <div className="App w-full">
-      <header className="App-header">
-        <h2 className="text-4xl">Movie Viewer</h2>
-      </header>
+const getTMDBData = async () => {
+    try {
+        const res = await axios.get(
+            `
+      https://api.themoviedb.org/3/trending/movie/week?api_key=${tmdb_api_key}`
+        );
 
-        <Poster data={"test"} />
-    </div>
-  );
+        let { results } = res.data;
+
+        return results.map(p => "https://image.tmdb.org/t/p/w185/" + p.poster_path);
+    } catch (error) {
+        throw error;
+    }
+};
+
+function App() {
+    const [posters, setPosters] = useState([]);
+    useEffect(() => {
+        getTMDBData().then(data => {
+            setPosters(data);
+        });
+    }, []);
+    return (
+        <div className="App w-full">
+            <header className="flex flex-wrap my-4 mx-auto w-5/6">
+                <h2 className="text-4xl">Movie Viewer</h2>
+            </header>
+            <div class="flex flex-wrap my-4 mx-auto w-5/6 justify-between">
+                {posters.map(p => (
+                    <Poster data={p} />
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default App;
