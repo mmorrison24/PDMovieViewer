644a68b301dce6f1cf49d48c4cb69250cf9f3b70
Mathew Morrison <m@MacBook-Pro.local>
2019-12-10 07:48:45 -0500

    Initial Poster setup

--- movie-viewer/src/App.js
+++ movie-viewer/src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 
+const Poster = ({ data }) => (
+    <div
+        class="bg-gray-400 border m-2 rounded-lg shadow-md p-6"
+        style={{
+            width: "185px"
+        }}
+    >
+        <img src={data} alt="poster"></img>
+    </div>
+);
+
 function App() {
   return (
     <div className="App w-full">
       <header className="App-header">
         <h2 className="text-4xl">Movie Viewer</h2>
       </header>
+
+        <Poster data={"test"} />
     </div>
   );
 }
