f92045ce20149b405ef127063b5437b8f2fc11f0
Mathew Morrison <m@MacBook-Pro.local>
2019-12-10 07:48:58 -0500

    FronteEnd + Server connect + BFs

--- movie-viewer/src/App.js
+++ movie-viewer/src/App.js
@@ -3,16 +3,19 @@ import axios from "axios";
 
 const tmdb_api_key = "cd140517203d1d5fbed8108980313abc";
 
-const Poster = ({ data }) => (
+const Poster = ({ data }) => {
+    const handleClick = async tmdb_id => {
+        await axios.post("http://localhost:5000/movies", {tmdb_id});
+    };
+    return (
     <div
-        class=" border m-2 rounded-lg shadow-md p-3"
-        style={{
-            width: "200px"
-        }}
+        className=" border m-2 rounded-lg shadow-md p-3"
+        onClick={() => handleClick(data.id)}
+        style={{width: "200px"}}
     >
-        <img src={data} alt="poster"></img>
-    </div>
-);
+        <img src={data.poster} alt="poster"></img>
+    </div>)
+};
 
 const getTMDBData = async () => {
     try {
@@ -23,7 +26,10 @@ const getTMDBData = async () => {
 
         let { results } = res.data;
 
-        return results.map(p => "https://image.tmdb.org/t/p/w185/" + p.poster_path);
+        return results.map(p => ({
+            ...p,
+            poster: "https://image.tmdb.org/t/p/w185/" + p.poster_path
+        }));
     } catch (error) {
         throw error;
     }
@@ -41,9 +47,9 @@ function App() {
             <header className="flex flex-wrap my-4 mx-auto w-5/6">
                 <h2 className="text-4xl">Movie Viewer</h2>
             </header>
-            <div class="flex flex-wrap my-4 mx-auto w-5/6 justify-between">
-                {posters.map(p => (
-                    <Poster data={p} />
+            <div className="flex flex-wrap my-4 mx-auto w-5/6 justify-between">
+                {posters.map((p,i) => (
+                    <Poster data={p} key={i} />
                 ))}
             </div>
         </div>
